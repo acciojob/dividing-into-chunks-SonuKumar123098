f449ff55567dbc4834299cee2f94d3b0c5bc3bb5
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-25 19:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let res=[],subarr=[],trackSum=0;
+	for(int i=0;i<arr.length;i++){
+		if(arr[i]+trackSum<=n){
+			subarr.push(arr[i]);
+			trackSum+=arr[i];
+		}else{
+			res.push(subarr);
+			subarr=[ arr[i] ];
+			trackSum=arr[i];
+		}
+	}
+	res.push(subarr);
+	return res;
 };
 
 const n = prompt("Enter n: ");
